5c4c4fbde7b9b1cec18f4d293c99fc883ddd8a33
Ankit <138029836+anku92@users.noreply.github.com>
2024-06-08 13:13:09 +0530

    Update cinema_data.js

--- dummy_data/cinema_data.js
+++ dummy_data/cinema_data.js
@@ -1,53 +1,53 @@
 // CINEMA DATA WITH MVOIE IDS
 
 [
-    {
-        "id": 1,
-        "name": "Cineplex",
-        "city": "Metropolis",
-        "location": "123 Main Street",
-        "movies": [3,5,7,8,9]
-    },
-    {
-        "id": 2,
-        "name": "Star Cinemas",
-        "city": "Gotham",
-        "location": "456 Oak Avenue",
-        "movies": [2, 4, 6]
-    },
-    {
-        "id": 3,
-        "name": "Mega Movies",
-        "city": "Atlantis",
-        "location": "789 Pine Boulevard",
-        "movies": [1,5,8,10]
-    },
-    {
-        "id": 4,
-        "name": "Cineworld",
-        "city": "Zion",
-        "location": "101 Cedar Street",
-        "movies": [2,3,6,7,9]
-    },
-    {
-        "id": 5,
-        "name": "Super Cinemas",
-        "city": "Central City",
-        "location": "555 Elm Lane",
-        "movies": [1,2,4,6,9]
-    },
-    {
-        "id": 6,
-        "name": "Galaxy Theaters",
-        "city": "Star City",
-        "location": "876 Oak Street",
-        "movies": [3,5,7,8]
-    },
-    {
-        "id": 7,
-        "name": "Dreamland Cinema",
-        "city": "Olympus",
-        "location": "321 Cedar Avenue",
-        "movies": [1,4,6,9]
-    }
+  {
+    "id": 1,
+    "name": "Cineplex",
+    "city": "Metropolis",
+    "location": "123 Main Street",
+    "movies": [3, 5, 7, 8, 9]
+  },
+  {
+    "id": 2,
+    "name": "Star Cinemas",
+    "city": "Gotham",
+    "location": "456 Oak Avenue",
+    "movies": [1, 2, 4, 6, 10]
+  },
+  {
+    "id": 3,
+    "name": "Mega Movies",
+    "city": "Atlantis",
+    "location": "789 Pine Boulevard",
+    "movies": [5, 8, 10, 12]
+  },
+  {
+    "id": 4,
+    "name": "Cineworld",
+    "city": "Zion",
+    "location": "101 Cedar Street",
+    "movies": [2, 3, 6, 7, 9]
+  },
+  {
+    "id": 5,
+    "name": "Super Cinemas",
+    "city": "Central City",
+    "location": "555 Elm Lane",
+    "movies": [1, 2, 4, 6, 9]
+  },
+  {
+    "id": 6,
+    "name": "Galaxy Theaters",
+    "city": "Star City",
+    "location": "876 Oak Street",
+    "movies": [3, 5, 7, 8]
+  },
+  {
+    "id": 7,
+    "name": "Dreamland Cinema",
+    "city": "Olympus",
+    "location": "321 Cedar Avenue",
+    "movies": [1, 4, 6, 9]
+  }
 ]
